fa73b9193ad6732c2ce2a191c798e7f0a79664c5
khunamir <amir12102009@gmail.com>
2021-07-16 17:59:07 +0800

    Custom Header

--- routes/AboutStack.js
+++ routes/AboutStack.js
@@ -1,26 +1,43 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import About from "../screens/about";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
 
-const AboutNavigator = () => (
-    <Stack.Navigator 
-        screenOptions = {{
-            headerStyle: {
-                backgroundColor: "#eee",
-                size: 60
-            },
-            headerTintColor: "#444"
-        }}
-    >
-      <Stack.Screen name="About" 
-        component={About}
-        options={{
-            title: "About GameZone"
-        }}
-      />
-    </Stack.Navigator>
-);
+const AboutNavigator = ({ navigation }) => {
+
+    const openMenu = () => {
+        navigation.openDrawer();
+    }
+    
+    return (
+        <Stack.Navigator 
+            screenOptions = {{
+                headerTitleAlign: "center",
+                headerMode: "screen",
+                headerStyle: {
+                    backgroundColor: "#eee",
+                    height: 100
+                },
+                headerTintColor: "#444"
+            }}
+        >
+        <Stack.Screen 
+            name="About" 
+            component={About}
+            options={{
+                title: "About",
+                headerLeft: () => (
+                    <MaterialIcons name="menu" size={28} color='black' onPress={openMenu} />
+                ),
+                headerLeftContainerStyle: {
+                    paddingHorizontal: 20
+                }
+            }}
+        />
+        </Stack.Navigator>
+    );
+}
 
 export default AboutNavigator;
--- routes/HomeStack.js
+++ routes/HomeStack.js
@@ -2,32 +2,50 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../screens/home";
 import ReviewDetails from "../screens/reviewDetails";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
 
-const HomeNavigator = () => (
+const HomeNavigator = ({ navigation }) => {
+
+  const openMenu = () => {
+    navigation.openDrawer();
+  }
+
+  return (
     <Stack.Navigator 
-        screenOptions = {{
-            headerStyle: {
-                backgroundColor: "#eee",
-                size: 60
-            },
-            headerTintColor: "#444"
-        }}
+      headerMode= "screen"
+      screenOptions = {{
+          headerTitleAlign: "center",
+          headerStyle: {
+              backgroundColor: "#eee",
+              height: 100
+          },
+          headerTintColor: "#444"
+      }}
     >
-      <Stack.Screen name="Home" 
-        component={Home}
+      <Stack.Screen 
+        name="Home" 
+        component={ Home }
         options={{
-            title: "GameZone"
+            title: "Home",
+            headerLeft: () => (
+                <MaterialIcons name="menu" size={28} color='black' onPress={openMenu} />
+            ),
+            headerLeftContainerStyle: {
+                paddingHorizontal: 20
+            }
         }}
       />
-      <Stack.Screen name="ReviewDetails"
+      <Stack.Screen 
+        name="ReviewDetails"
         component={ReviewDetails}
         options={{
             title: "Review Details"
         }}
       />
     </Stack.Navigator>
-);
+  )
+};
 
 export default HomeNavigator;
