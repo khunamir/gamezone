92e3291c813be7cad518a9241305f7c8538d43b5
khunamir <amir12102009@gmail.com>
2021-07-17 16:24:33 +0800

    Cards and Images

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { globalStyles } from '../styles/global';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { globalStyles, images } from '../styles/global';
+import Card from '../shared/card';
 
-export default function ReviewDetails({ navigation, route }) {
+export default function ReviewDetails({ route }) {
 
     const { title, body, rating } = route.params;
 
     return (
         <View style={ globalStyles.container }>
-            <Text>{ title }</Text>
-            <Text>{ body }</Text>
-            <Text>{ rating }</Text>
+            <Card>
+                <Text>{ title }</Text>
+                <Text>{ body }</Text>
+                <View style={ styles.rating }>
+                    <Text>GameZone rating: </Text>
+                    <Image style={{ width: rating*21, height: 21 }} source={ images.ratings[rating] } />
+                </View>
+            </Card>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    rating: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#eee'
+    }
+})
